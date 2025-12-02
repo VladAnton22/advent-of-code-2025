2827272179c9ebfac5049ba0660c68c4cd24df2c
Vlad Anton <182820291+VladAnton22@users.noreply.github.com>
2025-12-02 15:40:24 +0000

    Finished day01 part 2

--- day01/main.py
+++ day01/main.py
@@ -1,29 +1,49 @@
-# ------------- Part 1 ---------------
-
 mod = 100
 pointer = 50
-zero_counter = 0
+p1_counter = 0
+p2_counter = 0
 
-def change_pointer(current, instruction):
-    """Function that returns the new poisition of the dial after an instruction."""
+def process_rotation(start, instruction):
+    """Function that returns the number of zeros that get passed during each instruction"""
 
     direction = instruction[0]
-    value = int(instruction[1:])
+    steps = int(instruction[1:])
 
-    if direction.lower() == "r":
-        new_pointer = (current + value) % mod
-    elif direction.lower() == "l":
-        new_pointer = (current - value) % mod
+    if direction == "R":
+        dir_step = 1
+        first_k = -start % mod    # How many clicks until we get to 0?
+    else:   # L
+        dir_step = -1
+        first_k = start % mod
 
-    return new_pointer
+    zeros = 0
+
+    # Part 2: count zeros after each click
+    if first_k > 0 and first_k <= steps:
+        zeros += 1
+
+    zeros += max(0, (steps - first_k) // mod)
+
+    # Part 1: get final pointer
+    new_pointer = (start + dir_step * steps) % mod
+
+    return new_pointer, zeros
+
 
 with open('input.txt', 'r') as infile:
     lines = infile.readlines()
 
     for line in lines:
         line = line.strip()
-        pointer = change_pointer(pointer, line)
-        if pointer == 0:
-            zero_counter += 1
+        pointer, zeros_hit = process_rotation(pointer, line)
 
-print(zero_counter)
+        # Part 1: only count if pointer ends on 0
+        if pointer == 0:
+            p1_counter += 1
+
+        # Part 2
+        p2_counter += zeros_hit
+
+print(f"Part 1: {p1_counter}")     # 1089
+print(f"Part 2: {p2_counter}")     # 6530
+
